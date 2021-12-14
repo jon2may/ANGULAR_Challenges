7e5674343e4b2548442f65036938d8d1cb62841b
Jon2may <jonathan.demay004@gmail.com>
2021-12-14 13:50:37 +0100

    directives exercices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+
+  isThisIngredientVital:boolean = true;
+  displayGuestList:boolean = true;
+
+  songList:string[] = ['Chanson 1', 'Chanson 2', 'Chanson 3', 'Chanson 4'];
 }
